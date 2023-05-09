8e7dd03120fb782f54d9536c16907647cddac2fe
Simon Palmberg <simon.palmberg@gmail.com>
2023-05-09 10:38:03 +0200

    Updated modal story

--- src/components/Modal/Modal.stories.tsx
+++ src/components/Modal/Modal.stories.tsx
@@ -1,8 +1,9 @@
 import type { Meta, StoryObj } from '@storybook/react'
+import { useState } from 'react'
 import { alignItems, justifyContent } from 'src/storybookHelper'
 
 // import { buttonType, priority } from '../../storybookHelper'
-import { Modal } from './Modal'
+import { Modal, ModalProps } from './Modal'
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta: Meta<typeof Modal> = {
@@ -15,7 +16,7 @@ const meta: Meta<typeof Modal> = {
     alignItems,
     justifyContent,
     blur: { control: 'boolean' },
-    visible: { control: 'boolean' },
+    // visible: { control: 'boolean' },
     width: { control: 'string' },
     zIndex: { control: 'number' },
   },
@@ -24,20 +25,30 @@ const meta: Meta<typeof Modal> = {
 export default meta
 type Story = StoryObj<typeof Modal>
 
+const ModalWithState = (args: any) => {
+  const [visible, setVisible] = useState(false)
+  return (
+    <>
+      <button onClick={() => setVisible(true)}>Show modal</button>
+      <Modal {...args} visible={visible} onClose={() => setVisible(false)}>
+        <div
+          style={{ backgroundColor: '#FFF', padding: 'var(--spacing-xl)', borderRadius: '1rem' }}
+        >
+          <h3>This is a heading</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+            invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
+            accusam
+          </p>
+
+          <button>This is a button</button>
+        </div>
+      </Modal>
+    </>
+  )
+}
+
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 export const OneItem: Story = {
-  render: (args) => (
-    <Modal {...args}>
-      <div style={{ backgroundColor: '#FFF', padding: 'var(--spacing-xl)', borderRadius: '1rem' }}>
-        <h3>This is a heading</h3>
-        <p>
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
-          invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
-          accusam
-        </p>
-
-        <button>This is a button</button>
-      </div>
-    </Modal>
-  ),
+  render: (args) => <ModalWithState args={args} />,
 }
